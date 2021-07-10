e4bd8fa6baecda328c039eb44df603ee83287587
CPerez816 <Carlosocbjj@gmail.com>
2021-07-10 16:02:46 -0400

    :added button functions in js

---
+++ script.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelectorAll('[data-equals]')
+const deleteButton = document.querySelectorAll('[data-equals]')
+const allClearButton = document.querySelectorAll('[data-equals]')
+const equalsButton = document.querySelectorAll('[data-equals]')
+const equalsButton = document.querySelectorAll('[data-equals]')
+
